5e000870b6ad7431a26bab935d25df2c3cfa4a96
Imoto Yasuhiro <soyiharu@gmail.com>
2022-05-02 08:03:04 +0900

    動作確認に最低限必要なメッセージの送受信を作成

--- server/main.cpp
+++ server/main.cpp
@@ -5,113 +5,107 @@
 #include <grpcpp/health_check_service_interface.h>
 #pragma warning(pop)
 
+#include <condition_variable>
 #include <iostream>
 #include <memory>
+#include <mutex>
+#include <queue>
+#include <sstream>
 #include <string>
+#include <thread>
 
 #pragma warning(push)
 #pragma warning(disable : 4251 26451 26812 26495 6387 6385)
 #include "usi.grpc.pb.h"
 #pragma warning(pop)
 
-enum class Type { READ = 1, WRITE = 2, CONNECT = 3, DONE = 4, FINISH = 5 };
-
-class AsyncUsiProtocolServer
+class UsiProtocolServer final : public usi::UsiProtocol::Service
 {
 public:
-  AsyncUsiProtocolServer() {
-    // In general avoid setting up the server in the main thread (specifically,
-    // in a constructor-like function such as this). We ignore this in the
-    // context of an example.
-    std::string server_address("0.0.0.0:50051");
+  grpc::Status Communicate(grpc::ServerContext *context,
+                           grpc::ServerReaderWriter<usi::GuiMessage, usi::EngineMessage> *stream) override {
+    input_thread_ = std::make_unique<std::thread>(std::bind(&UsiProtocolServer::ReadStandardInput, this, stream));
 
-    grpc::ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service_);
-    cq_ = builder.AddCompletionQueue();
-    server_ = builder.BuildAndStart();
-
-    // This initiates a single stream for a single client. To allow multiple
-    // clients in different threads to connect, simply 'request' from the
-    // different threads. Each stream is independent but can use the same
-    // completion queue/context objects.
-    stream_ = std::make_unique<grpc::ServerAsyncReaderWriter<usi::GuiMessage, usi::EngineMessage>>(&context_);
-    service_.RequestCommunicate(&context_, stream_.get(), cq_.get(), cq_.get(),
-                                reinterpret_cast<void *>(Type::CONNECT));
-
-    // This is important as the server should know when the client is done.
-    context_.AsyncNotifyWhenDone(reinterpret_cast<void *>(Type::DONE));
-
-    grpc_thread_ = std::make_unique<std::thread>((std::bind(&AsyncUsiProtocolServer::GrpcThread, this)));
-    std::cout << "Server listening on " << server_address << std::endl;
-  }
-
-  ~AsyncUsiProtocolServer() {
-    std::cout << "Shutting down server...." << std::endl;
-    server_->Shutdown();
-    // Always shutdown the completion queue after the server.
-    cq_->Shutdown();
-    grpc_thread_->join();
-  }
-
-  void SetResponse(const std::string &response) {
-    if (response == "quit" && IsRunning()) {
-      stream_->Finish(grpc::Status::CANCELLED, reinterpret_cast<void *>(Type::FINISH));
+    usi::EngineMessage em;
+    while (stream->Read(&em)) {
+      switch (em.msg_case()) {
+      case usi::EngineMessage::MsgCase::kId: {
+        usi::EngineId id = em.id();
+        std::cout << "id name " << id.name() << std::endl;
+        std::cout << "id author " << id.author() << std::endl;
+      } break;
+      case usi::EngineMessage::MsgCase::kSm: {
+        usi::SingleMessage msg = em.sm();
+        switch (msg) {
+        case usi::ISREADY: std::cout << "isready" << std ::endl; break;
+        default: break;
+        }
+      } break;
+      default: break;
+      }
     }
-    response_str_ = response;
-  }
-  bool IsRunning() const { return is_running_; }
+    if (!exit_flag_) {
+      // GUIからのメッセージではなく、エンジンとの接続が切れてループを抜けたと思われる
+      // getlineで止まっていると思うので、スレッドを捨てる
+      input_thread_->detach();
+    } else {
+      input_thread_->join();
+    }
 
-  void GrpcThread() {
+    return grpc::Status::OK;
+  }
+
+  // 標準入力から読むこむ
+  void ReadStandardInput(grpc::ServerReaderWriter<usi::GuiMessage, usi::EngineMessage> *stream) {
+    std::string cmd, token;
+    std::string position;
     while (true) {
-      void *got_tag = nullptr;
-      bool ok = false;
-      if (!cq_->Next(&got_tag, &ok)) {
-        std::cerr << "Server stream closed. Quitting" << std::endl;
-        break;
+      if (!std::getline(std::cin, cmd)) {
+        cmd = "quit";
       }
 
-      // assert(ok);
+      std::istringstream iss(cmd);
 
-      if (ok) {
-        std::cout << std::endl << "**** Processing completion queue tag " << got_tag << std::endl;
-        switch (static_cast<Type>(reinterpret_cast<size_t>(got_tag))) {
-        case Type::READ:
-          std::cout << "Read a new message." << std::endl;
-          // AsyncHelloSendResponse();
-          break;
-        case Type::WRITE:
-          std::cout << "Sending message (async)." << std::endl;
-          // AsyncWaitForHelloRequest();
-          break;
-        case Type::CONNECT:
-          std::cout << "Client connected." << std::endl;
-          // AsyncWaitForHelloRequest();
-          break;
-        case Type::DONE:
-          std::cout << "Server disconnecting." << std::endl;
-          is_running_ = false;
-          break;
-        case Type::FINISH: std::cout << "Server quitting." << std::endl; break;
-        default: std::cerr << "Unexpected tag " << got_tag << std::endl; assert(false);
-        }
+      token.clear();
+      iss >> std::skipws >> token;
+
+      if (token == "quit") {
+        usi::GuiMessage msg;
+        msg.set_sm(usi::SingleMessage::QUIT);
+        stream->Write(msg);
+
+        exit_flag_ = true;
+        break;
+      } else if (token == "usi") {
+        usi::GuiMessage msg;
+        msg.set_sm(usi::SingleMessage::USI);
+        stream->Write(msg);
       }
     }
   }
 
 private:
-  usi::EngineMessage request_;
-  std::string response_str_ = "Default server response";
-  grpc::ServerContext context_;
-  usi::UsiProtocol::AsyncService service_;
-  std::unique_ptr<grpc::ServerCompletionQueue> cq_;
-  std::unique_ptr<grpc::Server> server_;
-  std::unique_ptr<grpc::ServerAsyncReaderWriter<usi::GuiMessage, usi::EngineMessage>> stream_;
-  std::unique_ptr<std::thread> grpc_thread_;
-  bool is_running_ = true;
+  std::mutex mutex_;
+  std::condition_variable cv_;
+  std::unique_ptr<std::thread> input_thread_;
+
+  bool exit_flag_ = false;
 };
 
+void RunServer() {
+  std::string server_address("0.0.0.0:50051");
+  UsiProtocolServer service;
+
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
 int main() {
-  AsyncUsiProtocolServer server;
+  RunServer();
+
   return 0;
 }
